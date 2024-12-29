5a890fceb2538436addab1c2ab25c1a7832e044e
Mohamed Sameh <hamadauter@gmail.com>
2024-12-29 14:07:37 +0300

    Exported the exerciselistitem function and made the code look cleaner

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import exercises from './assets/data/exercises.json'
+import ExerciseListItem from './src/components/ExerciseListItem';
+
+
 
 export default function App() {
-  const exercise = exercises[0]
-  console.log(exercise);
-
-
   return (
     <View style={styles.container}>
+      <FlatList 
+        data={exercises}
+        contentContainerStyle={{gap: 6}}
+        keyExtractor={(item) => item.name}
+        renderItem={({item}) => <ExerciseListItem item={item}/>}
+      />
 
-      <View style={styles.exerciseContainer}>
-        <Text style={styles.exerciseName}>{exercise.name}</Text>
-        <Text style={styles.exerciseSubtitle}>{exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}</Text>
-      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -25,18 +27,6 @@ const styles = StyleSheet.create({
     backgroundColor: 'gainsboro',
     justifyContent: 'center',
     padding: 10,
-  },
-  exerciseContainer: {
-    backgroundColor: '#fff',
-    padding: 10,
-    borderRadius: 15,
-    gap: 5,
-  },
-  exerciseName: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  exerciseSubtitle: {
-    color: 'dimgray',
+    paddingTop: 70,
   },
 });
