b72bff221250e84e8b031271ae8a3d5d38bf08ac
Mohamed Sameh <hamadauter@gmail.com>
2024-12-30 12:20:45 +0300

    Added expand instructions and some styling

--- src/app/[name].jsx
+++ src/app/[name].jsx
@@ -2,10 +2,12 @@ import { View, Text, StyleSheet, ScrollView } from "react-native"
 import { useLocalSearchParams } from "expo-router"
 import exercises from '../../assets/data/exercises.json'
 import {Stack} from "expo-router"
+import { useState } from "react"
 
 export default function ExerciseDetailsScreen() {
   const params = useLocalSearchParams()
 
+  const [isInstructionExpanded, setIsInstructionExpanded] = useState(false)
   const exercise = exercises.find(item => item.name === params.name)
 
   if(!exercise) {
@@ -25,9 +27,11 @@ export default function ExerciseDetailsScreen() {
     </View>
 
     <View style={styles.panel}>
-       <Text style={styles.instructions}>
+       <Text style={styles.instructions} numberOfLines={isInstructionExpanded ? 0 : 2}>
         {exercise.instructions}
        </Text>
+       <Text onPress={() => setIsInstructionExpanded(!isInstructionExpanded)} style={styles.seeMore}>
+        {isInstructionExpanded ? 'See less' : 'See more'}</Text>
     </View>
     </ScrollView>
   )
@@ -58,7 +62,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#f5f5f5',
     padding: 10,
     borderRadius: 10,
-
+  },
+  seeMore: {
+    alignSelf: 'center',
+    padding: 5,
+    fontWeight: '600',
+    color: 'gray',
   },
 });
 
